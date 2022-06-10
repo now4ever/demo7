e0ad68fb7e17b927b686032bb1dc9f0625e38ae9
now4ever <now4ever@naver.com>
2022-06-10 12:59:11 +0900

    컨트롤러, index 수정 2개 추가 by hong

--- demo3/src/main/java/com/ez/demo/controller/TestController.java
+++ demo3/src/main/java/com/ez/demo/controller/TestController.java
@@ -1,6 +1,13 @@
 package com.ez.demo.controller;
 
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class TestController {
-
+	
+	public String test() {
+		System.out.println("test by hong");
+		
+		return "/index";
+	}
 }
